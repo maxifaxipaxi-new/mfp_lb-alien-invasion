64e62afd3abef4ede271fe2e6ea0ce22c9c3ca1a
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-09-19 12:18:49 +0200

    Initial

---
+++ mfp_lb-alieninvasion/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.name = "Alien Invasion" -- Name of the App
+Config.description = "Alians are alive!" -- disc in the appstore (edit for your local)
+Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
+Config.size = 5932 -- OPTIONAL in kb
+Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
+
+
+-- Support on discord.gg/mfp if needed. Check out issues on github. --
